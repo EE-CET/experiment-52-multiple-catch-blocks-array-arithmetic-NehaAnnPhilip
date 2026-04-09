5e31f1114394bf2a2f2751ac6aa24f7ec3faa540
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-09 23:45:02 +0530

    Update MultipleCatch.java

--- src/MultipleCatch.java
+++ src/MultipleCatch.java
@@ -1,14 +1,44 @@
 import java.util.Scanner;
 
 public class MultipleCatch {
-    
-        // TODO: Read N
-        // TODO: Create array and read N integers
-        // TODO: Read index
-        // TODO: Read divisor
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        // TODO: 
+        try {
+            // TODO: Read N
+            if (!scanner.hasNextInt()) return;
+            int n = scanner.nextInt();
+            
+            // TODO: Create array and read N integers
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                if (scanner.hasNextInt()) {
+                    arr[i] = scanner.nextInt();
+                }
+            }
+            
+            // TODO: Read index
+            if (!scanner.hasNextInt()) return;
+            int index = scanner.nextInt();
+            
+            // TODO: Read divisor
+            if (!scanner.hasNextInt()) return;
+            int divisor = scanner.nextInt();
+            
+            // Perform division of the element at the specified index
+            int result = arr[index] / divisor;
+            System.out.println(result);
+            
+        } 
         // TODO: catch ArrayIndexOutOfBoundsException -> Print "Invalid Index"
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index");
+        } 
         // TODO: catch ArithmeticException -> Print "Divide by zero error"
-    
+        catch (ArithmeticException e) {
+            System.out.println("Divide by zero error");
+        }
+        
+        scanner.close();
+    }
 }
